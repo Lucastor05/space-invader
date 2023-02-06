7b35ee033e94a44839b511a859d7adfda2f586ed
Lucastor05 <lucasdeniel05@gmail.com>
2023-02-06 12:53:50 +0100

    v1

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const grille = document.querySelector(".grille");
 
-
-for (let pas = 0; pas <240; pas++) {
+for (let pas = 1; pas <241; pas++) {
 
     let newDiv = document.createElement("div");
 
     if(pas % 20 === 0){
-        newDiv.setAttribute("class","left_div");
-    }
-
-    if((pas+1) % 20 === 0){
         newDiv.setAttribute("class","right_div");
     }
 
-    if(pas === 230){
-        newDiv.classList.add("tireur");
+    if((pas-1) % 20 === 0){
+        newDiv.setAttribute("class","left_div");
     }
 
-    newDiv.setAttribute("id",pas);
-    
     grille.appendChild(newDiv);
 }
 
+
+var tableauId = [0,1,2,3,4,5,6,7,8,9,10,11,20,21,22,23,24,25,26,27,28,29,30,31,40,41,42,43,44,45,46,47,48,49,50,51];
+
+
+const tableauGrille = grille.children;
+
+tableauGrille[230].classList.add("tireur");
+
+
+/*var newArray = tableauId;
+newArray.forEach(element => {
+    if(newArray[element].classList == "right_div"){
+        tableauId.forEach(element => {
+            tableauId[element]+=20;
+        });
+    }else{
+        tableauId.forEach(element => {
+            tableauId[element]+=1;
+        });
+    }
+});*/
+
+
+tableauId.forEach(element => {
+    tableauGrille[element].classList.add("alien");
+});
 
 
