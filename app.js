c98b3e22959a23cff8ac0bc0caf20df2e8c24423
Lucastor05 <lucasdeniel05@gmail.com>
2023-02-06 16:29:56 +0100

    alien bouge done

--- app.js
+++ app.js
@@ -1,4 +1,58 @@
 const grille = document.querySelector(".grille");
+const tableauGrille = grille.children;
+
+function removeAlien(tableau, grid){
+    tableau.forEach(element => {
+        grid[element].classList.remove("alien");
+    });
+}
+
+function setAlien(tableau, grid){
+    tableau.forEach(element => {
+        grid[element].classList.add("alien");
+    });
+}
+
+function moveAlien(){
+    removeAlien(alien, tableauGrille);
+
+    var isRight = false;
+    var isLeft = false;
+
+    alien.forEach(element => {
+        if(tableauGrille[element].classList.contains("right_div") && !wasOnSide){
+            isRight = true;
+        }else if(tableauGrille[element].classList.contains("left_div") && !wasOnSide){
+            isLeft = true;
+        }
+    });
+
+
+    if(isRight || isLeft){
+        for (let i = 0; i < alien.length; i++) {
+            alien[i]+=20;
+        }
+
+        if(isRight){
+            direction = -1;
+        }else{
+            direction = 1;
+        }
+        console.log(direction)
+        wasOnSide = true;
+        
+    }else{
+        for (let i = 0; i < alien.length; i++) {
+            alien[i]+=direction;
+            wasOnSide = false
+        }
+    }
+
+    setAlien(alien, tableauGrille);
+}
+
+
+
 
 for (let pas = 1; pas <241; pas++) {
 
@@ -16,30 +70,50 @@ for (let pas = 1; pas <241; pas++) {
 }
 
 
-var tableauId = [0,1,2,3,4,5,6,7,8,9,10,11,20,21,22,23,24,25,26,27,28,29,30,31,40,41,42,43,44,45,46,47,48,49,50,51];
 
-
-const tableauGrille = grille.children;
 
 tableauGrille[230].classList.add("tireur");
+var alien = [0,1,2,3,4,5,6,7,8,9,10,11,20,21,22,23,24,25,26,27,28,29,30,31,40,41,42,43,44,45,46,47,48,49,50,51];
+var direction = 1;
+var wasOnSide = true;
 
 
-/*var newArray = tableauId;
-newArray.forEach(element => {
-    if(newArray[element].classList == "right_div"){
-        tableauId.forEach(element => {
-            tableauId[element]+=20;
-        });
-    }else{
-        tableauId.forEach(element => {
-            tableauId[element]+=1;
-        });
-    }
-});*/
+setAlien(alien, tableauGrille);
 
 
-tableauId.forEach(element => {
-    tableauGrille[element].classList.add("alien");
-});
+var interval = setInterval(moveAlien, 100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
