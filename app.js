2a65472c8432b2b362bce5601f9a54dd5d18c20a
sylv0r <sylvian.vidal95@gmail.com>
2023-02-07 10:01:19 +0100

    er

--- app.js
+++ app.js
@@ -94,8 +94,6 @@ var interval = setInterval(moveAlien, 1000);
 
 
 
-var tableauId = [0,1,2,3,4,5,6,7,8,9,10,11,20,21,22,23,24,25,26,27,28,29,30,31,40,41,42,43,44,45,46,47,48,49,50,51];
-
 
 
 tableauGrille[230].classList.add("tireur");
