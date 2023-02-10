c510e120b38e77576b2ace2694567d583d779ff4
Lucastor05 <lucasdeniel05@gmail.com>
2023-02-10 09:00:31 +0100

    v3

--- market.js
+++ market.js
@@ -19,24 +19,24 @@ submitBtn.addEventListener("click", function(event) {
   });
 
 
-  const ultraze = document.querySelector("#ultraze");
+const ultraze = document.querySelector("#ultraze");
 
-  ultraze.addEventListener("click", function(event) {
-      event.preventDefault();
+ultraze.addEventListener("click", function(event) {
+    event.preventDefault();
     
-      var bestscore = JSON.parse(localStorage.getItem("bestScores")) || {};
-      var user = sessionStorage.getItem("user");
-      var score =0;
-      for (username in bestscore) {   
-          if (username == user) {
-              score = bestscore[username];
-          }
-      }
-      if (parseInt(score) > 3000) {
-          sessionStorage.setItem("ultraze", user);
-      }else{
-          alert("Vous n'avez pas assez de points pour acheter cette bombe");
-      }
+    var bestscore = JSON.parse(localStorage.getItem("bestScores")) || {};
+    var user = sessionStorage.getItem("user");
+    var score =0;
+    for (username in bestscore) {   
+        if (username == user) {
+            score = bestscore[username];
+        }
+    }
+    if (parseInt(score) > 3000) {
+        sessionStorage.setItem("ultraze", user);
+    }else{
+        alert("Vous n'avez pas assez de points pour acheter cette bombe");
+    }
     });
     
   
